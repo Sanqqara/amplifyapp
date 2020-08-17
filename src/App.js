2d9ac01222538c53d882336e8ca1710d05ef49c8
Tim Sankara <chimpy.the.dev@outlook.com>
2020-08-18 02:09:08 +0300

    Revert "adding aws auth"
    This reverts commit 01ef7016530d9b609187a371f13fc88dc781f6eb.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
 
 function App() {
   return (
@@ -14,11 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
       </header>
-      <div>
-        <AmplifySignOut/>
-      </div>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
